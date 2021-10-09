357b25919f0c2bc3d23d24e8a3f602f11aa00f34
Nenonymo <nchentre666@gmail.com>
2021-10-09 02:50:58 +0200

    a star implementation

--- pathfinding.py
+++ pathfinding.py
@@ -2,10 +2,6 @@
 
 from sys import argv
 
-try:
-    inputFile = argv[1]
-except IndexError:
-    inputFile = None
 
 
 class Direction:
@@ -15,9 +11,6 @@ class Direction:
 
     def move(self, location):
         return(location[0]+self.alpha[0], location[1]+self.alpha[1])
-
-
-
 
 class State:
     def __init__(self, size, obstacles, location, cost, action, parent, goal):
@@ -33,17 +26,27 @@ class State:
         childs = []
         for dir_ in directions:
             newLocation = dir_.move(self.location) #new coordinates of the system
-
             if newLocation in self.obstacles: continue #bump into an obstacle
             elif newLocation == self.parent.location: continue #revert the last action
-
-            childs.append(State(size=self.size, obstacles=self.obstacles, location=newLocation, cost=self.cost+1, action=dir_, parent=self, goal=self.goal)) #add the new state
+            childs.append(State(size=self.size, obstacles=self.obstacles, location=newLocation, cost=self.cost+1, action=dir_, parent=self, goal=self.goal, path=self.path)) #add the new state
+        return childs
     
     def __eq__ (self, other):
-        return(self.location == self.location)
+        return(self.location == other.location)
 
     def score(self):
         return(self.cost + abs(self.location[0] - self.goal[0]) + abs(self.location[1] - self.goal[1]))
+
+    def give_path(self):
+        truePath = []
+        p = self
+        while p.action != None:
+            truePath.insert(0, p.action)
+            p = p.parent
+        return(truePath)
+
+    def achieved_state(self):
+        return(self.location == self.goal)
 
 class InvertedPriorityQueue():
     def __init__(self):
@@ -70,8 +73,50 @@ def a_star(model: State):
     queue = InvertedPriorityQueue()
     queue.insert(model.score(), model) #Insert to the queue the first state
 
+    while not queue.is_empty():
+        state = queue.delete()
+
+        if state.achievedState():
+            return(state.path)
+        
+        for child in state.successors():
+            queue.insert(child.score(), child)
+        
+    return(None)
+
+def create_world_model(inputFile): #input file parsing to create the initial world model
+    obstacles = []
+    location = ()
+    goal = ()
+    size = [0, 0]
+    with open(inputFile, mode='r', encoding="utf-8") as f:
+        lines = f.readlines()
+        size[0] = len(lines) #get the map size (height)
+
+    for l in range(len(lines)):
+        if size[1] < len(lines[l]): size[1] = len(lines[1]) #get the max size of the map (weidth)
+        for c in range(len(lines[l])):
+            if lines[l][c] == '#': obstacles.append((l, c)) #wall, house, water etc
+            elif lines[l][c] == '.': goal = (l, c) #goal
+            elif lines[l][c] == '@': location = (l, c) #system location
+            elif lines[l][c] == '+': #system on the goal
+                location = (l, c)
+                goal = (l, c)
+            if(goal == () or location == ()): raise ValueError("You need to specifiy 1 goal and 1 system position in the map")
+    
+    return(State(size=size, obstacles=obstacles, location = location, cost=0, action=None, parent=None, goal=goal))
 
 
+##### Treatment pipeline
+try:
+    inputFile = argv[1]
+except IndexError:
+    inputFile = None
 
-
+wM = create_world_model(inputFile)
+bestPath = a_star(wM)
+if bestPath == None:
+    print("No path would be found")
+else:
+    print(bestPath)
 
