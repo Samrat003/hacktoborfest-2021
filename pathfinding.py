8e37d7f0ee3efe95e477f1d48720c1e21040de57
Nenonymo <nchentre666@gmail.com>
2021-10-09 04:04:52 +0200

    everything's working

--- pathfinding.py
+++ pathfinding.py
@@ -11,6 +11,11 @@ class Direction:
     def move(self, location):
         return(location[0]+self.alpha[0], location[1]+self.alpha[1])
 
+UP = Direction("NORTH", (-1, 0))
+DOWN = Direction("SOOUTH", (1, 0))
+RIGHT = Direction("EAST", (0, 1))
+LEFT = Direction("WEST", (0, -1))
+
 class State:
     def __init__(self, size, obstacles, location, cost, action, parent, goal):
         self.size = size
@@ -21,17 +26,16 @@ class State:
         self.parent = parent
         self.goal = goal
 
-    def succesors(self, directions): #generate all the child states from the current state
+    def successors(self, directions=(UP, DOWN, LEFT, RIGHT)): #generate all the child states from the current state
         childs = []
         for dir_ in directions:
             newLocation = dir_.move(self.location) #new coordinates of the system
-            if newLocation in self.obstacles: continue #bump into an obstacle
-            elif newLocation == self.parent.location: continue #revert the last action
-            childs.append(State(size=self.size, obstacles=self.obstacles, location=newLocation, cost=self.cost+1, action=dir_, parent=self, goal=self.goal, path=self.path)) #add the new state
+            if (newLocation in self.obstacles) or (newLocation[0] not in range(self.size[0] or newLocation[1] not in range(self.location[1]))): continue #bump into an obstacle
+            childs.append(State(size=self.size, obstacles=self.obstacles, location=newLocation, cost=self.cost+1, action=dir_, parent=self, goal=self.goal)) #add the new state
         return childs
-    
-    def __eq__ (self, other):
-        return(self.location == other.location)
+
+    def __hash__ (self):
+        return(hash(self.location))
 
     def score(self):
         return(self.cost + abs(self.location[0] - self.goal[0]) + abs(self.location[1] - self.goal[1]))
@@ -48,6 +52,7 @@ class State:
         return(self.location == self.goal)
     
     def draw_state(self):
+        print(self.location, self.score())
         map_ = [[' ' for x in range(self.size[1])] for y in range(self.size[0])]
 
         for (y, x) in self.obstacles: map_[y][x] = '#'
@@ -55,12 +60,15 @@ class State:
         if self.goal == self.location: map_[self.location[0]][self.location[1]] = '+'
         else: map_[self.goal[0]][self.goal[1]] = '.'
 
+        for line in map_:
+            print(" ".join(line))
+
 
 class InvertedPriorityQueue():
     def __init__(self):
         self.queue = []
     
-    def insert(self, data, priority):
+    def insert(self, priority, data):
         self.queue.append([priority, data])
     
     def is_empty(self):
@@ -78,16 +86,18 @@ class InvertedPriorityQueue():
             return item
 
 def a_star(model: State):
+    table = []
     queue = InvertedPriorityQueue()
     queue.insert(model.score(), model) #Insert to the queue the first state
-
-    while not queue.is_empty():
-        state = queue.delete()
-
-        if state.achievedState():
-            return(state.path)
+    table.append(model.location)
+    while not queue.is_empty(): #if there still is some states to explore
+        state = queue.delete()[1] #grab the state with the lowest score
+        if state.achieved_state():
+            return(state.give_path())
         
         for child in state.successors():
+            if child.location in table: continue #security, in order not to explore the same state multiple times
+            table.append(child.location)
             queue.insert(child.score(), child)
         
     return(None)
@@ -104,13 +114,16 @@ def create_world_model(inputFile): #input file parsing to create the initial wor
     for l in range(len(lines)):
         if size[1] < len(lines[l]): size[1] = len(lines[1]) #get the max size of the map (weidth)
         for c in range(len(lines[l])):
-            if lines[l][c] == '#': obstacles.append((l, c)) #wall, house, water etc
-            elif lines[l][c] == '.': goal = (l, c) #goal
-            elif lines[l][c] == '@': location = (l, c) #system location
+            if lines[l][c] == '#':
+                obstacles.append((l, c)) #wall, house, water etc
+            elif lines[l][c] == '.':
+                goal = (l, c) #goal
+            elif lines[l][c] == '@':
+                location = (l, c) #system location
             elif lines[l][c] == '+': #system on the goal
                 location = (l, c)
                 goal = (l, c)
-            if(goal == () or location == ()): raise ValueError("You need to specifiy 1 goal and 1 system position in the map")
+    if(goal == () or location == ()): raise ValueError("You need to specifiy 1 goal and 1 system position in the map")
     
     return(State(size=size, obstacles=obstacles, location = location, cost=0, action=None, parent=None, goal=goal))
 
@@ -127,11 +140,12 @@ try:
 except IndexError:
     inputFile = None
 
-wM = create_world_model(inputFile)
+wM = create_world_model(inputFile) #Parsing the map to create the first world model
+wM.draw_state()
 location = [wM.goal, wM.location]
-bestPath = a_star(wM)
-if bestPath == None:
-    print("No path would be found")
+bestPath = a_star(wM) #launch A star and search for the best path
+if bestPath == None: #no paths
+    print("No path could be found")
 else:
-    print("{}->{} : [{}]".format(location[1], location[0], ", ".join(bestPath)))
+    print("{}->{} : [{}]".format(location[1], location[0], ", ".join(clean_path(bestPath))))
 
