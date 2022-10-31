54033b040aae5cfd362252ede55197602e111a9a
DESTROSANGRAM <92619946+DESTROSANGRAM@users.noreply.github.com>
2022-10-31 21:08:16 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,13 @@
+num = 11
+# If given number is greater than 1
+if num > 1:
+    # Iterate from 2 to n / 2
+    for i in range(2, int(num/2)+1):
+      
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
